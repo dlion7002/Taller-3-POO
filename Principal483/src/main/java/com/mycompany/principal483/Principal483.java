cdea568fe85ee10c0ec8159a96597761184e3f62
dlion7002 <jacobodleon@gmail.com>
2025-02-14 23:14:28 -0500

    Ejercicio 483

---
+++ Principal483/src/main/java/com/mycompany/principal483/Principal483.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.principal483;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Principal483 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true); 
+    }
+}
