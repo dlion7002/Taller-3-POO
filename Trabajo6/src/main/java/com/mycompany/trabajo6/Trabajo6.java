b9ff7a8ff3a52b4cdf6893cbafee29f748639ba1
dlion7002 <jacobodleon@gmail.com>
2025-03-04 22:33:30 -0500

    Trabajo 6

---
+++ Trabajo6/src/main/java/com/mycompany/trabajo6/Trabajo6.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajo6;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Trabajo6 {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true); 
+    }
+}
